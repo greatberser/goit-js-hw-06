39abc5738c19b87a1cb276e81bfd8b7643dfb3d9
Maks <maks135254@gmail.com>
2023-09-20 15:02:45 +0300

    done tasks 8&9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
 const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    const email = this.elements.email.value;
+    const password = this.elements.password.value;
+
+    if(!email || !password){
+        alert('Введіть значення в поля!');
+        return;
+    }
+
+    const formData = {
+        email,
+        password,
+    };
+
+    console.log(`Form data: ${formData}`);
+    this.reset(); 
+});
+
+
+
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+btnChangeColor.addEventListener("click", function(){
+  const randomColor = getRandomHexColor();
+
+  document.body.style.background = randomColor;
+  
+  spanColor.textContent = randomColor;
+});
