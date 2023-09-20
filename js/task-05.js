a54a7ac4534ee5d3eaeb00db30adf5cdccb71b39
Maks <maks135254@gmail.com>
2023-09-20 15:24:01 +0300

    done task 6

--- js/task-05.js
+++ js/task-05.js
@@ -3,4 +3,4 @@ const nameOutput = document.querySelector('#name-output');
 
 nameInput.addEventListener("input", (event) => {
     nameOutput.textContent = event.currentTarget.value;
-  });
+});
--- js/task-06.js
+++ js/task-06.js
@@ -1,4 +1,17 @@
 const validationInput = document.querySelector('#validation-input');
 
-validationInput.addEventListener("blur");
+validationInput.addEventListener("blur", () => {
+    const inputValue = validationInput.value.trim().length;
+
+
+    const expectedLength = parseInt(validationInput.getAttribute('data-length'));
+
+    if (inputValue === expectedLength){
+        validationInput.classList.remove('invalid');
+        validationInput.classList.add('valid');
+    } else {
+        validationInput.classList.remove('valid');
+        validationInput.classList.add('invalid');
+    }
+});
 
