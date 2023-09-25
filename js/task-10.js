71ea729641202cf40b5c38bec0a1ec6cee66c6d4
Maks <maks135254@gmail.com>
2023-09-25 11:29:37 +0300

    done task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const containerBoxes = document.querySelector('#boxes');
+
+createBtn.addEventListener("click", () => {
+  createBoxes(Number(input.value))
+});
+destroyBtn.addEventListener("click", removeBoxes);
+
+
+function createBoxes(amount) {
+  const containerBoxes = document.querySelector("#boxes");
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    const size = 30 + i * 10;
+    const color = getRandomHexColor();
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = color;
+
+    containerBoxes.appendChild(box);
+  }
+}
+
+function removeBoxes(){
+  containerBoxes.innerHTML = '';
+}
