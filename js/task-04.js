e1220a21ca482286c8c13358c14ae686abeb3b49
Maks <maks135254@gmail.com>
2023-09-26 16:27:31 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,15 +1,15 @@
-const counterValue = document.querySelector('#counter');
+const counterValue = document.querySelector('#value');
 let currentValue = 0;
 
 const decrementValue = document.querySelector('#counter button[data-action="decrement"]');
 const incrementValue = document.querySelector('#counter button[data-action="increment"]');
 
 decrementValue.addEventListener("click", () =>{
-    currentValue += 1;
+    currentValue -= 1;
     counterValue.textContent = currentValue;
 });
 
 incrementValue.addEventListener("click", () =>{
-    currentValue -= 1;
+    currentValue += 1;
     counterValue.textContent = currentValue;
-})
+});
